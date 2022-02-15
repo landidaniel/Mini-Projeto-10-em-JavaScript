38eba9cea67d8b0541b876830affa6fe7fbf9d4d
Daniel Landi Macário <landi_daniel@hotmail.com>
2022-02-15 11:21:32 -0300

    Add files via upload

---
+++ scripts10.js
@@ -0,0 +1,22 @@
+// Criar função para mostrar os números primos
+
+
+// Primos
+// Compostos
+
+exibirNumerosPrimos(35);
+
+function exibirNumerosPrimos(limite){
+    for(let numero = 2; numero <= limite; numero ++){
+        let ehPrimo = true;
+
+        for(let divisor = 2; divisor< numero; divisor++){
+            if(numero % divisor === 0 ){
+                ehPrimo = false;
+                break;
+            }
+        }
+
+        if(ehPrimo) console.log(numero);
+    }
+}
